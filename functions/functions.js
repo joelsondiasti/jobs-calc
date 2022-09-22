d9f56244f79b20b2cc76afbddb140f3b483adc4a
Joelson Dias <joelsondiasti@outlook.com>
2022-09-22 11:45:16 -0300

    commit inicial

---
+++ functions/functions.js
@@ -0,0 +1,10 @@
+function handleUserClick(valor) {
+  //alert("Você clicou em Adicionar novo job");
+  var total = document.getElementById("total");
+  total.innerHTML = valor;
+}
+
+// // O nome da função está em camelcase
+// function nomeDaMinhaFuncao(parametros) {
+//   //Corpo
+// }
